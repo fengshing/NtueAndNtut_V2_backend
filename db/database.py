602c5f887f4975958e7af6a6af9e266ee57cd5ab
fengshing <g111234017@grad.ntue.edu.tw>
2023-12-28 18:10:32 +0800

    first commit

---
+++ db/database.py
@@ -0,0 +1,28 @@
+# 1203新增，導入sqlalchemy後，直接複製講義的代碼，用來宣告SQLite資料庫形式
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+
+# SQLALCHEMY_DATABASE_URL = "sqlite:///./worklistdatabase.db"
+
+# 這個要用相對路徑，因為engine在app.py內被實作，所以資料庫檔案會建立在與app.py同⼀層的專案根⽬錄位置
+SQLALCHEMY_DATABASE_URL = "postgresql://default:z6LVURNXixS8@ep-jolly-base-17897110.us-east-1.postgres.vercel-storage.com:5432/verceldb"
+
+#  有docker幫忙命名後，URL規則是://${{USER}}:${{PASSWORD}}@${{HOST}}:${{PORT}/${{database}}
+# SQLALCHEMY_DATABASE_URL = "postgresql://worklistbe:givemebe@postgre:5432/worklist"
+
+
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
